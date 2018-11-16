08e72afb76f8597a68738788dc4ee514a16bbc9a
Atlas <achille@heraud.xyz>
2018-11-16 00:34:53 -0600

    Moved from multiple worker threads to multiple worker processes

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #!/usr/local/bin/python3
 
 import sys
-from queue import Queue
-from threading import Thread, local
-from collections import deque
+from multiprocessing import Pool
 
 from config import Config
 from cgp.functionset import FunctionSet
@@ -27,55 +25,56 @@ def constrain(eightBitArray):
     return eightBitArray / 255.0
 
 
+#env = None
+#config = None
+#function_set = None
+
+
+def worker_init(rom_path: str):
+    global env
+    global function_set
+    global config
+    env = Environment(rom_path)
+    function_set = FunctionSet()
+    config = Config()
+    config.inputs = 3
+    config.outputs = NUM_KEYS * 2  # because we have booleans
+    config.functionGenes = 40
+
+
+def play_game(genome: Genome):
+    key_presses = [False] * int(config.outputs / 2)
+    env.start_episode()
+    for _ in range(FRAMES_TO_SKIP_AT_START):
+        env.run_frame()
+
+    while env.is_running():
+        pixels = np.array(env.get_pixels())
+        pixels = pixels.reshape((env.HEIGHT, env.WIDTH))
+        rPixels = constrain((pixels >> 24) & 255)
+        gPixels = constrain((pixels >> 16) & 255)
+        bPixels = constrain((pixels >> 8) & 255)
+
+        output = genome.evaluate(rPixels, gPixels, bPixels)
+
+        for i in range(NUM_KEYS):
+            shouldPressKey = output[i] > output[i + 1]
+            env.set_key_state(i + 1, shouldPressKey)
+            key_presses[i] = shouldPressKey
+        for _ in range(FRAMES_TO_SKIP_EACH_TURN):
+            env.run_frame()
+
+    score = env.get_score()
+    print("score: ", score)
+
+    return (genome, score)
+
 def main():
     if len(sys.argv) < 2:
         print("Missing rom path argument.")
         return
 
     tetris_rom_path = sys.argv[1]
-
-    work_queue = Queue()  # queue of genomes to evaluate
-    result_queue = deque()  # queue of (genome, score) tuples
-
-    def worker():
-        env = Environment(tetris_rom_path)
-        while True:
-            genome = work_queue.get(True, None)
-
-            env.start_episode()
-            for _ in range(FRAMES_TO_SKIP_AT_START):
-                env.run_frame()
-
-            while env.is_running():
-                pixels = np.array(env.get_pixels())
-                pixels = pixels.reshape((height, width))
-                rPixels = constrain((pixels >> 24) & 255)
-                gPixels = constrain((pixels >> 16) & 255)
-                bPixels = constrain((pixels >> 8) & 255)
-
-                output = genome.evaluate(rPixels, gPixels, bPixels)
-
-                for i in range(NUM_KEYS):
-                    shouldPressKey = output[i] > output[i + 1]
-                    env.set_key_state(i + 1, shouldPressKey)
-                    keyPresses[i] = shouldPressKey
-                for _ in range(FRAMES_TO_SKIP_EACH_TURN):
-                    env.run_frame()
-
-            score = env.get_score()
-            print("score: ", score)
-            result_queue.append((genome, score))
-
-            work_queue.task_done()
-
-    # create some worker threads
-    thread_count = 4
-    for _ in range(thread_count):
-        Thread(target=worker).start()
-
-    env = Environment(tetris_rom_path)
-    width = env.WIDTH
-    height = env.HEIGHT
 
     functionSet = FunctionSet()
     config = Config()
@@ -86,23 +85,20 @@ def main():
 
     elite = Genome(config, functionSet)
     bestScore = 0
-    counter = 0.0
 
-    for generation in range(config.generations):
-        for _ in range(config.childrenPerGeneration):
-            work_queue.put(elite.get_child(), True)
+    with Pool(processes=4, initializer=worker_init, initargs=(tetris_rom_path,)) as pool:
 
-        # wait for all of the children to be evaluated
-        work_queue.join()
+        for generation in range(config.generations):
+            genomes = [elite.get_child() for _ in range(config.childrenPerGeneration)]
 
-        for (genome, score) in result_queue:
-            if score > bestScore:
-                bestScore = score
-                elite = genome
+            results = [pool.apply_async(play_game, args=(genome,)) for genome in genomes]
+            results = [result.get() for result in results]
+            for (genome, score) in results:
+                if score > bestScore:
+                    bestScore = score
+                    elite = genome
 
-        result_queue.clear()
-
-        print('Generation ' + str(generation) + ' end, current best score = ', bestScore)
+            print('Generation ' + str(generation + 1) + ' end, current best score = ', bestScore)
     print("FINISHED")
     print(bestScore)
 
