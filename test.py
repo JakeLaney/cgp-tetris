ee663695abd6b8aa17fadb67b4a1c86aae509283
Jacob Laney <jake.laney96@gmail.com>
2018-12-05 08:39:24 -0600

    fixing file saves and loads, previously did not include outputs

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 from config import Config
 from cgp.functionset import FunctionSet
 from cgp.genome import Genome
+from math import floor
 
 
 functionSet = FunctionSet()
@@ -11,4 +12,8 @@ config.outputs = 10 # because we have booleans
 config.functionGenes = 40
 keyPresses = [False] * int(config.outputs / 2)
 
-elite = Genome(config, functionSet)
+genome = Genome(config, functionSet)
+genome.save_to_file('jake.out')
+next = Genome(config, functionSet)
+next.load_from_file('jake.out')
+print(genome.evaluate(1,2,3))    
